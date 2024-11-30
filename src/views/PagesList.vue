eec4244037af576a57cf4fce8e00717cda5b99f1
Mahdi Ihsan <mahdiihsan16110@gmail.com>
2024-11-30 23:11:14 +0600

    Providing and Injecting Dependencies INTO Components

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,3 +1,38 @@
 <template>
-  <p>This is the pages List</p>  
+  <
+  <p>{{ data.counter }}</p>
+  <button @click.prevent ="data.counter++">Click</button>
 </template>
+
+
+<script setup>
+import {ref,reactive} from 'vue'
+
+// // const counter = ref(0); // {value:0};
+
+
+// const data = ref(0); // {value:0};
+const data = reactive({counter: 0});
+
+function click(){
+  // counter.value++;
+    data.counter++;
+
+}
+
+
+
+// export default{
+//     data(){
+//         return {
+//           counter: 0
+//         };
+//     },
+//     methods:{
+//       click(){
+//         this.counter++;
+//       }
+//     }
+// }
+
+</script>
